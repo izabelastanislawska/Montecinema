3e50adde6a920651c87450b368206f6b1f21ee47
Izabela Stanisławska <izabela.stanislawska@monterail.com>
2022-03-25 13:58:32 +0100

    refactioring MovieCard - slot

---
+++ src/components/MovieCard2.vue
@@ -0,0 +1,27 @@
+<template>
+    <section :id="idName" :class="`${idName}`">
+        <div>
+            <slot name="image" :class="`${idName}-image`"></slot>
+        </div>
+        <div :class="`${idName}-header`">
+            <slot name="header"></slot>
+        </div>
+        
+        <div :class="`${idName}-genre`">
+            <slot name="genre"></slot>
+        </div>
+        <div :class="`${idName}-length`">
+            <slot name="length"></slot>
+        </div>
+        <div :class="`${idName}-screenings`">
+            <slot name="screenings"></slot>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "MovieCard2",
+    props: ['idName'],
+}
+</script>
