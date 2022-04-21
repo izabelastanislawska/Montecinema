f0af3964aa1030e8c63a6bd76df7afe72cae1974
Izabela Stanisławska <izabela.stanislawska@monterail.com>
2022-04-21 16:45:23 +0200

    fixes

--- tests/unit/components/PasswordInput.spec.js
+++ tests/unit/components/PasswordInput.spec.js
@@ -4,18 +4,18 @@ import PasswordInput from '@/components/PasswordInput.vue';
 const createComponent = () => mount(PasswordInput);
 
 const findEyeButton = (wrapper) => wrapper.find("button");
-const findInputPassword = (wrapper) => wrapper.find("input[type='password']");
-const findInputText = (wrapper) => wrapper.find("input[type='text']");
+const findInput = (wrapper) => wrapper.find("input");
+// const findInputText = (wrapper) => wrapper.find("input[type='text']");
 
 
 describe('PasswordInput.vue', () => {
   it('shows password on eye-button click', async () => {
     const wrapper = createComponent();
     
-    expect(findInputPassword(wrapper).text()).toBe("password")
+    expect(findInput(wrapper).attributes('type')).toBe('password')
 
     await findEyeButton(wrapper).trigger("click")
 
-    expect(findInputText(wrapper).text()).toBe("text")
+    expect(findInput(wrapper).attributes('type')).toBe('text')
   }); 
 });
